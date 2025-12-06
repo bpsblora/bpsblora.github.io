bfa7587244f96c0172760e47e7f840aef9b2aae1
bpsblora <145369885+bpsblora@users.noreply.github.com>
2025-12-06 10:10:52 +0700

    Add files via upload

---
+++ pelayanan/pelayanan.js
@@ -0,0 +1,79 @@
+pelayanan_json = [
+			{
+				"nama": "Buku Tamu PST",
+				"desc": "",
+				"ikon": "https://imgur.com/ETutpro.png",
+			  	"url": "https://perpustakaan.bps.go.id/digilib/guestbook"
+			},
+			{
+				"nama": "Survei Kebutuhan Data",
+				"desc": "",
+				"ikon": "https://imgur.com/QIxdF7v.png",
+			  	"url": "https://skd.bps.go.id/SKD2025/web/entri/responden/blok1"
+			},
+			{
+				"nama": "PPID BPS Kabupaten Blora",
+				"desc": "",
+			  	"ikon": "https://imgur.com/6bqPrrF.png",
+			  	"url": "https://ppid.bps.go.id/?mfd=3316"
+			},
+			{
+				"nama": "Indonesia Datahub",
+				"desc": "",
+				"ikon": "https://imgur.com/0bkKnK2.png",
+			  	"url": "https://indah.bps.go.id/",
+			  	"tim": "g-sektoral",
+			},
+			{
+				"nama": "Romantik",
+				"desc": "",
+				"ikon": "https://imgur.com/kfOSIvo.png",
+			  	"url": "https://romantik.web.bps.go.id/",
+			  	"tim": "g-sektoral",
+			},
+			{
+				"nama": "Silastik",
+				"desc": "",
+				"ikon": "https://imgur.com/ixXaHDO.png",
+			  	"url": "https://silastik.bps.go.id/",
+			},
+			{
+				"nama": "SateBlora",
+				"desc": "",
+				"ikon": "https://imgur.com/UXO59Bf.png",
+			  	"url": "https://satudatablora.vercel.app/"
+			},
+			{
+				"nama": "Jatikuat",
+				"desc": "",
+				"ikon": "https://imgur.com/PgAtgVI.png",
+			  	"url": "https://webapps.bps.go.id/blorakab/jatikuat/"
+			},
+			{
+				"nama": "Website BPS Blora",
+				"desc": "",
+				"ikon": "https://imgur.com/NsgOjWL.png",
+			  	"url": "https://blorakab.bps.go.id/"
+			},
+			{
+				"nama": "Website BPS Jawa Tengah",
+				"desc": "",
+				"ikon": "https://imgur.com/uoMnvR4.png",
+			  	"url": "https://jateng.bps.go.id/"
+			},
+			{
+				"nama": "Website BPS RI",
+				"desc": "",
+				"ikon": "https://imgur.com/z1QD8fY.png",
+			  	"url": "https://bps.go.id/"
+			},
+			{
+				"nama": "PST BPS",
+				"desc": "",
+				"ikon": "https://imgur.com/7sZxErP.png",
+			  	"url": "https://pst.bps.go.id/"
+			}
+			
+
+			];
+
---
+++ pelayanan/pengaduan.js
@@ -0,0 +1,53 @@
+pengaduan_json = [
+			{
+				"nama": "SP4N Lapor",
+				"desc": "",
+				"ikon": "https://imgur.com/1GYa5CF.png",
+			  	"url": "https://www.lapor.go.id/"
+			},
+			{
+				"nama": "Instagram",
+				"desc": "",
+				"ikon": "https://imgur.com/FLDZ8az.png",
+			  	"url": "https://www.instagram.com/bpskabblora/"
+			},
+			{
+				"nama": "Facebook",
+				"desc": "",
+				"ikon": "https://imgur.com/wEhKmsQ.png",
+			  	"url": "https://web.facebook.com/bpskabblora/"
+			},
+			{
+				"nama": "Twitter",
+				"desc": "",
+				"ikon": "https://imgur.com/JSh0Wec.png",
+			  	"url": "https://twitter.com/bpskabblora"
+			},
+			{
+				"nama": "Youtube",
+				"desc": "",
+				"ikon": "https://imgur.com/Rxs11bV.png",
+			  	"url": "https://www.youtube.com/channel/UCmxx5zRjxpgTbjfwUPVUSKg"
+			},
+			{
+				"nama": "Tiktok",
+				"desc": "",
+				"ikon": "https://imgur.com/iD42jlO.png",
+			  	"url": "https://www.tiktok.com/@bpskabblora"
+			},
+			{
+				"nama": "Threads",
+				"desc": "",
+				"ikon": "https://imgur.com/aFktEob.png",
+			  	"url": "https://www.threads.net/@bpskabblora"
+			},
+			{
+				"nama": "Live Chat Whatsapp",
+				"desc": "",
+				"ikon": "https://imgur.com/hHTBwvy.png",
+			  	"url": "https://wa.me/+6282121330016"
+			}
+			
+
+			];
+
