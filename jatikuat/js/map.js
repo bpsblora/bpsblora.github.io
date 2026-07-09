f485b27ace684f9c58590d4e675c8e7bd4174827
bpsblora <145369885+bpsblora@users.noreply.github.com>
2026-07-09 15:08:02 +0700

    Update map.js

--- jatikuat/js/map.js
+++ jatikuat/js/map.js
@@ -1,34 +1,32 @@
 // BASEMAP
-            // BASEMAP
 var overlayMaps = {};
 
 var googleSat = L.tileLayer('https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 });
+
 var googleStreets = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 });
+
 var googleHybrid = L.tileLayer('https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}', {
     maxZoom: 20,
     subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
 });
 
-const BPNBasemap = L.TileLayer.extend({
-  createTile: function (coords, done) {
-    const tile = document.createElement('img');
-    tile.setAttribute('referrerpolicy', 'no-referrer');
-    L.DomEvent.on(tile, 'load', L.Util.bind(this._tileOnLoad, this, done, tile));
-    L.DomEvent.on(tile, 'error', L.Util.bind(this._tileOnError, this, done, tile));
-    tile.src = this.getTileUrl(coords);
-    return tile;
-  }
+// ALTERNATIF 1: Esri World Imagery (Paling tajam, mirip citra drone/foto udara detail)
+var esriImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 20,
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
 });
-var googleTerrain = new BPNBasemap('https://petadasar.atrbpn.go.id/main/wms/{x}/{y}/{z}', {
-    maxZoom: 19,
-    maxNativeZoom: 19,
-    attribution: '© Kementerian ATR/BPN - Model Dasar Pertanahan'
+
+// ALTERNATIF 2: Google Terrain Asli (Menampilkan kontur elevasi, bukan foto udara)
+var googleTerrain = L.tileLayer('https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}', {
+    maxZoom: 20,
+    subdomains: ['mt0', 'mt1', 'mt2', 'mt3'],
+    attribution: '© Google'
 });
 
 // Initialize Leaflet Map -- HARUS sebelum layer apapun di-addTo(map)
@@ -40,12 +38,12 @@ var map = L.map('map', {
         zoomControl: false,
         attributionControl: true
 });
-map.attributionControl.addAttribution(" BPS Kabupaten Blora");
+map.attributionControl.addAttribution("BPS Kabupaten Blora");
 
 // Basemap default yang tampil saat load
 googleSat.addTo(map);
 
-// Layer group untuk filter
+// Layer group untuk filter batas wilayah (Kecamatan, Desa, SLS)
 var layerKec = L.geoJson(null, {style:{
                             color:"blue", weight: 2, opacity: 0.5,
                             fillOpacity: 0.2, fillColor: 'blue'
@@ -59,14 +57,14 @@ var layerSls = L.geoJson(null, {style: {
                             fillOpacity: 0.2, fillColor: 'red',
                         }}).addTo(map);
 
+// Masukkan ke dalam control layers
 var baseMaps = {
-    "Satelit": googleSat,
-    "Terain atr": googleTerrain,
-    "Street": googleStreets,
+    "Satelit (Google)": googleSat,
+    "Citra Detail (Esri)": esriImagery, // Menggantikan posisi map ATR/BPN
+    "Topografi (Terrain)": googleTerrain,
+    "Jalan (Street)": googleStreets,
     "Hibrid": googleHybrid
 };
 
 L.control.zoom({ position: 'bottomleft' }).addTo(map);
 L.control.layers(baseMaps, overlayMaps, { position: 'bottomleft' }).addTo(map);
-
-         
